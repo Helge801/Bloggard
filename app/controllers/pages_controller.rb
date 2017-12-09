614bfc28a188287c1a3f47a2438adc19c5e715f5
Bryan Lund <bryan.helge@gmail.com>
2017-12-09 16:54:26 -0700

    basic erb setup for pages and controllor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,22 @@
 class PagesController < ApplicationController
+  before_action :get_blogs, only: [:titles, :subjects]
+
   def home
   end
 
   def authors
-    
+    @authors = Author.all
   end
 
   def titles
   end
 
   def subjects
+  end
+
+  private
+
+  def get_blogs
+    @blogs = Blog.all
   end
 end
