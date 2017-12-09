a837979253b459f2defbba1610c0928edf670339
Bryan Lund <bryan.helge@gmail.com>
2017-12-09 10:57:05 -0700

    generated blogs resource, pages controller, author model

---
+++ db/migrate/20171209174435_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.string :sub_title
+      t.string :subject
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
